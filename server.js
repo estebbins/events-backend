e99869c4f6069d953632de048b4373e31b5c2a11
estebbins <stebbins.e@gmail.com>
2023-02-21 17:18:39 -0500

    import dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+require("dotenv").config()
 
 // require route files
 const eventRoutes = require('./app/routes/event_routes')
